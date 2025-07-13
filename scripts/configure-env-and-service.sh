45dfa90a22ca04902fd0f18d723b6ef02cfd7ebc
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2025-07-14 05:37:40 +0600

    ci: attempt to fix CI (again)

---
+++ scripts/configure-env-and-service.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+APP_NAME="telekilogram"
+APP_DESC="Feed assistant"
+APP_DIR="/srv/${APP_NAME}"
+ENV_PATH="${APP_DIR}/.env"
+SERVICE_PATH="/etc/systemd/system/${APP_NAME}.service"
+
+# configure environment
+
+echo "TOKEN=\"${TOKEN}\"" > ${ENV_PATH}
+echo "ALLOWED_USERS=\"${ALLOWED_USERS}\"" >> ${ENV_PATH}
+
+# create service
+
+cat << EOF > "$SERVICE_PATH"
+[Unit]
+Description=$APP_DESC
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+ExecStart=$APP_DIR/app
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# grant permissions
+
+chmod 644 "$SERVICE_PATH"
+
+# enable and restart service
+
+systemctl daemon-reload
+systemctl enable "$SERVICE_PATH"
+systemctl restart "$SERVICE_PATH"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,21 +3,12 @@
 set -euxo pipefail
 
 APP_NAME="telekilogram"
-APP_DESC="Feed assistant"
 APP_DIR="/srv/${APP_NAME}"
-ENV_PATH="${APP_DIR}/.env"
-SERVICE_PATH="/etc/systemd/system/${APP_NAME}.service"
 
 # create app dir
 
 ssh ${SSH_USER}@${SSH_IP} -p ${SSH_PORT} \
     "mkdir -p ${APP_DIR}"
-
-# create and fill .env
-
-ssh ${SSH_USER}@${SSH_IP} -p ${SSH_PORT} \
-    "echo 'TOKEN=\"${TOKEN}\"' > ${ENV_PATH}; \
-    echo 'ALLOWED_USERS=\"${ALLOWED_USERS}\"' >> ${ENV_PATH}"
 
 # transfer binary
 
@@ -25,28 +16,7 @@ rsync -avzr --progress -e "ssh -p ${SSH_PORT}" \
     ./build/app \
     ${SSH_USER}@${SSH_IP}:${APP_DIR}/
 
-# create service
+# configure environment and service
 
-cat << EOF > "$SERVICE_PATH"
-[Unit]
-Description=$APP_DESC
-After=network.target
-
-[Service]
-Type=simple
-Restart=always
-ExecStart=$APP_DIR/app
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# grant permissions
-
-chmod 644 "$SERVICE_PATH"
-
-# enable and restart service
-
-systemctl daemon-reload
-systemctl enable "$SERVICE_PATH"
-systemctl restart "$SERVICE_PATH"
+ssh ${SSH_USER}@${SSH_IP} -p ${SSH_PORT} \
+    "bash -s" < ./scripts/configure-env-and-service.sh
