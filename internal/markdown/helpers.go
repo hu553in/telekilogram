a820326b282580baf2236faaab3f57181e07eda7
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2026-01-10 21:29:33 +0600

    fix: fix markdown.EscapeV2(...) logic

--- internal/markdown/helpers.go
+++ internal/markdown/helpers.go
@@ -5,12 +5,20 @@ import "strings"
 // Taken from https://core.telegram.org/bots/api#markdownv2-style.
 const mdV2SpecialChars = `._[](){}#|!+-=*~>` + "`"
 
+//nolint:gochecknoglobals // Lookup table meant to be immutable.
+var mdV2Lookup = func() [256]bool {
+	var m [256]bool
+	for i := range len(mdV2SpecialChars) {
+		m[mdV2SpecialChars[i]] = true
+	}
+	return m
+}()
+
 func EscapeV2(input string) string {
-	lookup := mdV2SpecialCharLookup()
 	charsToEscape := 0
 
-	for i := range input {
-		if lookup[input[i]] {
+	for i := range len(input) {
+		if mdV2Lookup[input[i]] {
 			charsToEscape++
 		}
 	}
@@ -21,21 +29,13 @@ func EscapeV2(input string) string {
 	var b strings.Builder
 	b.Grow(len(input) + charsToEscape)
 
-	for i := range input {
+	for i := range len(input) {
 		c := input[i]
-		if lookup[c] {
+		if mdV2Lookup[c] {
 			b.WriteByte('\\')
 		}
 		b.WriteByte(c)
 	}
 
 	return b.String()
-}
-
-func mdV2SpecialCharLookup() [256]bool {
-	var m [256]bool
-	for _, c := range []byte(mdV2SpecialChars) {
-		m[c] = true
-	}
-	return m
 }
